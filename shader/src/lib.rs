4c2e1498c29ef7d16e3af87005e57d6c5b4b2fc3
JoshuaBatty <joshpbatty@gmail.com>
2019-10-09 08:36:46 +1100

    pushing missing files from last PR

--- shader/src/lib.rs
+++ shader/src/lib.rs
@@ -1,10 +1,11 @@
 //! The shader function hotloaded at runtime by the cohen_gig crate.
 
 use nannou::prelude::*;
-use shader_shared::{Light, Uniforms, Vertex};
+use shader_shared::{Light, Uniforms, Vertex, MixingInfo};
 
 mod signals;
 mod helpers;
+mod blend_modes;
 mod shaders;
 
 mod solid_hsv_colour;
@@ -15,9 +16,35 @@ mod led_shaders;
 mod wash_shaders;
 
 #[no_mangle]
-fn shader(v: Vertex, uniforms: &Uniforms, shader_name: &String) -> LinSrgb {
+fn shader(v: Vertex, uniforms: &Uniforms, mix: &MixingInfo) -> LinSrgb {
+    let left = render_shader(v, &uniforms, &mix.left_name);
+    let right = render_shader(v, &uniforms, &mix.right_name);
+    let colour = render_shader(v, &uniforms, &mix.colour_name);
+
+    let xfl = lin_srgb(mix.xfade_left,mix.xfade_left,mix.xfade_left);
+    let xfr = lin_srgb(mix.xfade_right,mix.xfade_right,mix.xfade_right);
+
+    let mut col = match mix.blend_mode.as_str() {
+        "Add" => blend_modes::add(left*xfl, right*xfr) * colour,
+        "Subtract" => blend_modes::subtract(left*xfl, right*xfr) * colour,
+        "Multiply" => blend_modes::multiply(left, right) * colour,
+        "Average" => blend_modes::average(left*xfl, right*xfr) * colour,
+        "Difference" => blend_modes::difference(left*xfl, right*xfr) * colour,
+        "Negation" => blend_modes::negation(left*xfl, right*xfr) * colour,
+        "Exclusion" => blend_modes::exclusion(left*xfl, right*xfr) * colour,
+        _ => colour,
+    };
+
+    if let Light::Wash { .. } = v.light {
+        col = crate::helpers::lerp_lin_srgb(v.last_color, col, v.lerp_amt);
+    }
+
+    col
+}
+
+fn render_shader(v: Vertex, uniforms: &Uniforms, shader_name: &String) -> LinSrgb {
     let p = v.position;
-    let mut col = match shader_name.as_ref() {
+    match shader_name.as_ref() {
         "SolidHsvColour" => solid_hsv_colour::shader(v, uniforms),
         "SolidRgbColour" => solid_rgb_colour::shader(v, uniforms),
 
@@ -43,13 +70,5 @@ fn shader(v: Vertex, uniforms: &Uniforms, shader_name: &String) -> LinSrgb {
         // "MitchWash" => wash_shaders::mitch_wash::shader(v, uniforms),
         // "ColourPalettes" => wash_shaders::colour_palettes::shader(v, uniforms),
         _ => lin_srgb(0.0, 0.0, 0.0)
-    };
-
-    if let Light::Wash { .. } = v.light {
-        // TODO: Make this a slider parameter.
-        let lerp_amt = 1.0;
-        col = crate::helpers::lerp_lin_srgb(v.last_color, col, lerp_amt);
     }
-
-    col
 }
