f29d4f50f49966a08df8ad71e872832bc274b0a3
mitchmindtree <mitchell.nordine@gmail.com>
2019-10-10 20:57:40 +0200

    Commit config with presets. Add shader and preset with button demo.
    This commit includes the config.json into the gitignore so that we can
    start committing presets. I've added 6 or 7 presets to kick things off,
    but feel free to rename/remove them or whatevs if you have a flow in
    mind for the night!
    
    This also re-adds in the original wash test shader I added and uses it
    to demonstrate the affect of button presses. You can trigger the radial
    glow burst effect by pressing the cycle button on the korg (or spacebar
    which pretends to be the cycle button). This is just to give an idea of
    how it can potentially spice up the shaders a bit and add a bit of
    musical/spatial interactivity :+1:

--- shader/src/wash_shaders/mitch_wash.rs
+++ shader/src/wash_shaders/mitch_wash.rs
@@ -1,5 +1,5 @@
 use nannou::prelude::*;
-use shader_shared::{Uniforms, Vertex, Light};
+use shader_shared::{Button, Uniforms, Vertex, Light};
 
 use crate::helpers::*;
 
@@ -8,16 +8,23 @@ struct Params {
 }
 
 pub fn shader(v: Vertex , uniforms: &Uniforms) -> LinSrgb {
-    let params = Params {
-        speed: 0.5,
-    };
-
+    let speed = uniforms.params.mitch_wash.speed;
     let p = v.position;
-
-    let t = uniforms.time * params.speed;
+    let t = uniforms.time * speed;
     let b = (p.z + t).sin() * 0.5 + 0.5;
     let r = (p.x + t * 2.0 * p.x.signum()).cos() * 0.5 + 0.5;
     let g = (p.y + t).cos() * 0.5 + 0.5;
-    let col = vec3(b*r*0.5, g*b, b);
+    let mut col = vec3(b*r*0.5, g*b, b);
+
+    // Add a burst of light emanating from the led wall down the venue on cycle press.
+    if let Some(state) = uniforms.buttons.get(&Button::Cycle) {
+        let env = (1.0 - state.secs).max(0.0).powf(2.0);
+        let m = p.magnitude();
+        let dist = (m - state.secs * 4.0).abs();
+        let l = (1.0 - dist * 2.0).max(0.0);
+        let glow = l * env;
+        col += vec3(1.0, 1.0, 1.0) * glow;
+    }
+
     lin_srgb(col.x, col.y, col.z)
 }
